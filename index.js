0bd7ac45a74adb93abf516dc1d6bee4046b894fd
CepreNGT <malitevs@gmail.com>
2025-03-04 16:16:10 +0500

    Tasks 1, 2

--- index.js
+++ index.js
@@ -11,10 +11,36 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getComments() {
+    let comments = [];
+    for (const file of files) {
+        let index = 0;
+        while (index < file.length) {
+            let comment_index = index + file.substring(index).indexOf("// TODO ");
+            if (comment_index === index - 1) {
+                break;
+            }
+            let n_index = comment_index + file.substring(comment_index).indexOf("\r\n");
+            if (n_index === comment_index - 1) {
+                n_index = file.length;
+            }
+            comments.push(file.substring(comment_index, n_index));
+            index = n_index;
+        }
+    }
+    return comments;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            const comment = getComments();
+            for (let i = 0 ; i < comment.length; i++) {
+                console.log(comment[i]);
+            }
             break;
         default:
             console.log('wrong command');
