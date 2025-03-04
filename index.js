1f99dddfc3f5dd7a19d4ab11efdf1d076e4d3dbf
CepreNGT <malitevs@gmail.com>
2025-03-04 17:26:04 +0500

    task 5

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 const comments = getComments();
+getSortedComments('user')
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -13,15 +14,15 @@ function getFiles() {
 }
 
 function getComments() {
-    let comments = [];
+    const comments = [];
     for (const file of files) {
         let index = 0;
         while (index < file.length) {
-            let comment_index = index + file.substring(index).indexOf("// TODO ");
+            const comment_index = index + file.substring(index).indexOf('// TODO ');
             if (comment_index === index - 1) {
                 break;
             }
-            let n_index = comment_index + file.substring(comment_index).indexOf("\r\n");
+            let n_index = comment_index + file.substring(comment_index).indexOf('\r\n');
             if (n_index === comment_index - 1) {
                 n_index = file.length;
             }
@@ -32,8 +33,72 @@ function getComments() {
     return comments;
 }
 
+function parseCommentsToObject() {
+    const commentsObject = {};
+    commentsObject[0] = [];
+    for (const comment of comments) {
+        if (comment.split(';').length !== 3) {
+            commentsObject[0].push(comment);
+        } else {
+            username = comment.split(';')[0].substring(8).toLowerCase();
+            if (!commentsObject[username]) {
+                commentsObject[username] = [];
+            }
+            commentsObject[username].push(comment);
+        }
+    }
+    return commentsObject;
+}
+
+function count(str, symbol) {
+    let count = 0;
+    for (let char of str) {
+        if (char === symbol) {
+            count++;
+        }
+    }
+    return count;
+};
+
+function comparer(a, b) {
+    if (a.split(';').length !== 3) {
+        return false;
+    }
+    if (b.split(';').length !== 3) {
+        return true;
+    }
+    const dateA = new Date(a.split(';')[1].trim());
+    const dateB = new Date(b.split(';')[1].trim());
+    return dateB - dateA;
+}
+
+function getSortedComments(arg) {
+    switch (arg) {
+        case 'importance':
+            return comments.concat().sort((a, b) => count(b, '!') - count(a, '!'));
+        case 'user':
+            const result = [];
+            const commentsObject = parseCommentsToObject();
+            for (const key of Object.keys(commentsObject)) {
+                if (key === '0') {
+                    continue;
+                }
+                for (const comm of commentsObject[key]) {
+                    result.push(comm);
+                }
+            }
+            for (const comm of commentsObject[0]) {
+                result.push(comm);
+            }
+            return result;
+        case 'date':
+            return comments.concat().sort(comparer);
+    }
+}
+
 function processCommand(command) {
-    switch (command) {
+    const parsedCommand = command.split(' ');
+    switch (parsedCommand[0]) {
         case 'exit':
             process.exit(0);
             break;
@@ -44,9 +109,14 @@ function processCommand(command) {
             break;
         case 'important':
             for (const comment of comments) {
-                if (comment.indexOf("!") !== -1) {
+                if (comment.indexOf('!') !== -1) {
                     console.log(comment);
                 }
+            }
+            break;
+        case 'sort':
+            for (const comment of getSortedComments(parsedCommand[1])) {
+                console.log(comment);
             }
             break;
         default:
